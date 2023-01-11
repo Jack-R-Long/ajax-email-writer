da7d73fcf5f7b787b7ccb1b66466ae90466487f2
Jack Long <jack.richard.long@gmail.com>
2023-01-11 11:44:00 -0600

    test process env on edge API

--- pages/api/edge-route.ts
+++ pages/api/edge-route.ts
@@ -4,10 +4,6 @@ export const config = {
   runtime: "experimental-edge",
 };
 
-interface Env {
-    TEST_VAR: string;
-}
-
 export default async function (req: NextRequest) {
   return new Response(JSON.stringify({ name: "John Doe", node_version: process.env.NODE_VERSION }), {
     status: 200,
