677154862c8c74f9a2ff31870a42789cb67c4c80
Jack Long <jack.richard.long@gmail.com>
2023-01-11 11:27:45 -0600

    test env vars on the edge

--- pages/api/edge-route.ts
+++ pages/api/edge-route.ts
@@ -4,8 +4,12 @@ export const config = {
   runtime: "experimental-edge",
 };
 
+interface Env {
+    TEST_VAR: string;
+}
+
 export default async function (req: NextRequest) {
-  return new Response(JSON.stringify({ name: "John Doe" }), {
+  return new Response(JSON.stringify({ name: "John Doe", node_version: process.env.NODE_VERSION }), {
     status: 200,
     headers: {
       "Content-Type": "application/json",
