c49f12b9b6f0997148ef8d69d472c8f235d2e3da
Jack Long <jack.richard.long@gmail.com>
2023-01-19 20:53:10 -0600

    get rid of edge router config and longer input

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -1,23 +1,10 @@
 import { Configuration, OpenAIApi } from "openai";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export const config = {
-  runtime: "experimental-edge",
-};
-
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-
-interface OpenAiError extends Error {
-  response: {
-    status: number;
-    data: {
-      error: string;
-    };
-  }; 
-}
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
   if (!configuration.apiKey) {
@@ -58,7 +45,8 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
       model: "text-davinci-003",
       prompt: generateEmailPrompt(recipient, company, description, sentences),
       temperature: 0.7,
-      max_tokens: 256,
+      max_tokens: 1000,
+      frequency_penalty: 1.0,
     });
     console.log(completion.data)
     res.status(200).json({ result: completion.data.choices[0].text });
